1018a796bc6240d8e23bf7bebf001c47a33c244f
Nur Kholid <cocokcocok33@gmail.com>
2023-05-27 17:44:39 +0700

    fix content md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
@@ -7,7 +7,12 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react(), mdx()]
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    react(),
+    mdx(),
+  ],
 });
